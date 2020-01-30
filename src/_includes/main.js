1c33abf6ee9d3e7cbf5ac919486a55e7b3b4f591
Simon Opheim <simon.opheim@attentec.no>
2020-01-30 20:16:53 +0100

    feat: close drawer when clicking outside of menu

--- src/_includes/main.js
+++ src/_includes/main.js
@@ -1,4 +1,19 @@
 const menuToggle = document.querySelector('.menu__toggle');
 const menu = document.querySelector('.menu');
+let closed = true;
 
-menuToggle.addEventListener('click', () => menu.classList.toggle('menu--open'));
+function closeMenuOnOutsideClick({ target }) {
+  if (target.className === 'menu__toggle' || closed) {
+    return
+  }
+  toggleMenu();
+}
+
+function toggleMenu() {
+  menu.classList.toggle('menu--open');
+  closed = !closed;
+}
+
+
+menuToggle.addEventListener('click', toggleMenu);
+document.addEventListener('click', closeMenuOnOutsideClick);
