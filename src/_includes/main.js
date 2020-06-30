296bf2b67277bf31d7461de59cc69fb6567753f6
Simon Opheim <simon.opheim@gmail.com>
2020-06-30 17:28:21 +0200

    feat: add tabs to portfolio page

--- src/_includes/main.js
+++ src/_includes/main.js
@@ -1,6 +1,8 @@
 const menuToggle = document.querySelector('.menu__toggle');
 const menu = document.querySelector('.menu');
 const menuCover = document.querySelector('.menu__cover');
+const tabButtons = document.querySelectorAll('.portfolio__tab');
+
 let closed = true;
 
 function toggleMenu() {
@@ -9,5 +11,23 @@ function toggleMenu() {
   closed = !closed;
 }
 
+function openTab(event) {
+  const { textContent } = this;
+  const tabContent = document.getElementsByClassName('portfolio__section');
+
+  for (let i = 0; i < tabContent.length; i++) {
+    tabContent[i].style.display = 'none';
+  }
+
+  for (i = 0; i < tabButtons.length; i++) {
+    tabButtons[i].classList.remove('portfolio__tab--active');
+  }
+
+  event.currentTarget.classList.add('portfolio__tab--active');
+  document.getElementById(textContent).style.display = 'block';
+  document.getElementById('Hobbyprosjekter').classList.add('animate--left');
+}
+
 menuToggle.addEventListener('click', toggleMenu);
 menuCover.addEventListener('click', toggleMenu);
+tabButtons.forEach((tab) => tab.addEventListener('click', openTab));
